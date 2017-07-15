0fda069a7197b4f4deffbb15e22e004a31952fd0
Erik <erikdesjardins@users.noreply.github.com>
2017-07-15 13:06:36 -0400

    don't need jquery or bootstrap's js

--- static/js/analytics.js
+++
@@ -1,5 +0,0 @@
-$('.browsers').on('click', 'a[data-browser]', function(e) {
-    var browser = $(e.currentTarget).data('browser');
-    var action = 'install' + browser.charAt(0).toUpperCase() + browser.slice(1);
-    ga('send', 'event', 'userscript', action);
-});
