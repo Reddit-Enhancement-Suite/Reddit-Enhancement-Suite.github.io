580f3cab0cdf7f31f8e43012a023f5a5d985354e
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-08-25 11:09:05 -0400

    silence curl stats

--- invalidate.sh
+++ invalidate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl "https://www.cloudflare.com/api_json.html" \
+curl -sS "https://www.cloudflare.com/api_json.html" \
   -d "a=fpurge_ts" \
   -d "tkn=$CLOUDFLARE_API_KEY" \
   -d "email=$CLOUDFLARE_EMAIL" \
