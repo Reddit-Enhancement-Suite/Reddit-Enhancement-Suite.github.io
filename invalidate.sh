ee5cd4fc0e17adcdaa8c2394dfe5721440207798
Erik <erikdesjardins@users.noreply.github.com>
2018-05-16 16:59:10 -0400

    remove cloudflare cache invalidation
    since we use native github pages https/cdn now

--- invalidate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sS "https://www.cloudflare.com/api_json.html" \
-  -d "a=fpurge_ts" \
-  -d "tkn=$CLOUDFLARE_API_KEY" \
-  -d "email=$CLOUDFLARE_EMAIL" \
-  -d "z=redditenhancementsuite.com" \
-  -d "v=1"
